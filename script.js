693cbef7eb6581d0d9e08614f16fde7c194bf25e
animesh3640 <127398605+animesh3640@users.noreply.github.com>
2023-04-28 16:59:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const p = document.getElementById("timer");
+function timer(){
+    const date = new Date();
+    var time=`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    var dateString=`${date.getMonth()+1}/${date.getDay()}/${date.getFullYear()}, ${time.toLocaleString()} `
+    p.innerText=dateString
+    // console.log(dateString)
+}
+setInterval(timer,1000)
